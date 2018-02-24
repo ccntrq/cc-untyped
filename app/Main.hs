09141a09edcb99eb3ac85728802690c3160bfba8
Alex Pankoff <ccntrq@screenri.de>
2018-02-25 00:14:13 +0100

    add Church numbers and bools

--- app/Main.hs
+++ app/Main.hs
@@ -6,12 +6,25 @@ import           Syntax
 
 main :: IO ()
 main =
-  let terms = ["((lambda a.a)(lambda a.a))", "\\a.\\b.\\c.\\d.a b c d"]
-  in mapM_ (\x -> smallStep x >> bigStep x) terms
+  let terms =
+        [ "((lambda a.a)(lambda a.a))"
+        , "\\a.\\b.\\c.\\d.a b c d"
+        , tru
+        , fls
+        , test
+        , c0
+        , c1
+        , c2
+        , c3
+        , test ++ tru ++ c3 ++ c0
+        , test ++ fls ++ c0 ++ c3
+        ]
+  in mapM_ smallStep terms
 
 pprint :: String -> IO ()
 pprint = either print printTerm . parse
 
+-- TODO: add possibility to choose
 bigStep :: String -> IO ()
 bigStep src =
   case parse src of
@@ -25,7 +38,27 @@ bigStep src =
 smallStep :: String -> IO ()
 smallStep src =
   case parse src of
-    Left err -> print err
+    Left err -> putStrLn src >> print err
     Right tm -> do
       evalWithM (putStrLn . ("Reducing: \n" ++) . fmtTerm []) [] tm
       putStrLn "Done."
+
+{-
+ - Church Booleans
+ -}
+tru = "(\\t.\\f.t)"
+
+fls = "(\\t.\\f.f)"
+
+test = "(\\l.\\m.\\n.l m n)"
+
+{-
+ - Church Numbers
+ -}
+c0 = "\\s.\\z.z"
+
+c1 = "\\s.\\z.s z"
+
+c2 = "\\s.\\z.s (s z)"
+
+c3 = "\\s.\\z.s (s (s z))"
